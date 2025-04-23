1b29acc2369d80ead08749f4e2f3583c7ba73fa6
Shawon Ashraf <shawon.ashraf@proton.me>
2025-04-24 01:34:02 +0600

    example code

---
+++ example.py
@@ -0,0 +1,21 @@
+from visualiser import AttentionVisualiser
+from transformers import AutoModel, AutoTokenizer
+
+if __name__ == "__main__":
+    model_name = "openai-community/openai-gpt"
+
+    model = AutoModel.from_pretrained(model_name)
+    model.eval()
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    text = "Look on my Works, ye Mighty, and despair!"
+    encoded_inputs = tokenizer.encode_plus(
+        text,
+        truncation=True,
+        return_tensors="pt"
+    )
+
+
+    visualiser = AttentionVisualiser(model, tokenizer)
+    visualiser.visualise_attn_layer(0, encoded_inputs)
+
